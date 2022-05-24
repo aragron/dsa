b46b610edb9dfd4b32e0151bc886e2b13a9917cd
wangzh <wangzh02@asp.citicgz.com>
2022-05-24 21:37:10 +0800

    leetcode动态规划

---
+++ java/src/dsa/leetcode/BinarySearch.java
@@ -0,0 +1,26 @@
+package dsa.leetcode;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        int search = search(nums, target);
+        System.out.println(search);
+    }
+
+    public static int search(int[] nums, int target) {
+        int low =0, high = nums.length-1;
+        while (low <= high) {
+            int mid = (high-low)/2+low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            }else if (num > target) {
+                high = mid -1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
